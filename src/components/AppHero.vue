4d0d688cfbfa059385047c70f102834853a77dbd
“jdelavega94” <jcdelavega94@gmail.com>
2023-04-12 16:51:18 +0200

    project completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            phrase: "Powered by"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <p> {{ phrase }}</p>
+        <img src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style>
+.jumbotron {
+    background-color: lightcoral;
+    height: 25vh;
+    padding: 1rem;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
